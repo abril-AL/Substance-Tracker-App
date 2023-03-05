729096325313e2e7feca4392d3ddc8ff470892d9
abril-AL <abril_al@yahoo.com>
2023-03-04 16:47:19 -0800

    now displays day in word/writted form instead of YYYY-MM-DD, ex First of June

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,9 +1,9 @@
 import { Alert, StyleSheet } from 'react-native';
 import { Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+//import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { Component, useState, useEffect } from 'react';
-import { Calendar } from 'react-native-calendars';
-import Colors from '../constants/Colors';
+import { Calendar, DateData } from 'react-native-calendars';
+//import Colors from '../constants/Colors';
 
 //parent function
 function FuncScreen() {
@@ -52,8 +52,7 @@ function CAL(CalProps: any) {
         selectedDotColor: 'red',
         indicatorColor: 'blue',
       }}
-      onDayPress={day => { CalProps.setDay(day.dateString) }}
-      //onDayPress={day => CalProps.setDay(day)}
+      onDayPress={day => { CalProps.setDay(day.dateString), getLogs(day.dateString) }}
       minDate='2023-01-01'
       maxDate='2025-01-31'
       hideExtraDays={true}
@@ -68,7 +67,7 @@ function LogDisplay(LogDisplay: any) {
   //console.log(DAY);
   return (
     <View>
-      <Text style={styles.log}> Log Summary for {LogDisplay.day} </Text>
+      <Text style={styles.log}> Logs for the {getDayString(LogDisplay.day)} </Text>
     </View >
   )
 }
@@ -88,13 +87,13 @@ function helper() {
   return (year + '-' + addZeroMonth + month + '-' + addzeroDate + date);
 }
 //return the log for the passed day, returns a dictionary of info for that day
-function getLogs(day: any) {
-  const Y = day.substring(0, 4);
-  const M = day.substring(5, 7);
-  const D = day.substring(9);
-  console.log(D + ',' + M + ',' + Y);
-  const obj = DATA[Y][M][D];
-  //TODO, return the dict at DATA[Y[M[D]]]
+function getLogs(day: string) {
+  const components = day.split('-');
+  const Y = components[0];
+  const M = components[1];
+  const D = components[2];
+  console.log(DATA[Y][M][D]);
+  return (DATA[Y][M][D]);
 }
 
 //Important, returns the screen :o
@@ -103,8 +102,6 @@ export default FuncScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //alignItems: 'center',
-    //justifyContent: 'center',
   },
   title: {
     color: 'white',
@@ -135,26 +132,40 @@ type Substance = Record<string, number>;
 type Day = Record<string, Substance>;
 type Month = Record<string, Day>;
 type Year = Record<string, Month>;
-
 const DATA: Year = {
   '2023': {
-    '01': { '01': { 'mdma': 2 }, '03': { 'alcohol': 12 } },
+    '01': { '05': { 'MDMA': 2 }, '06': { 'Alcohol': 12 } },
     '02': {},
-    '03': { '01': { 'Marijuana': 1 }, '02': { 'Marijuana': 2 }, '03': { 'alcohol': 4, 'Marijuana': 1 }, '04': { 'alcohol': 4, 'Xanax': 1 } },
+    '03': { '01': { 'Marijuana': 1 }, '02': { 'Marijuana': 2 }, '03': { 'Alcohol': 4, 'Marijuana': 1 }, '04': { 'Alcohol': 4, 'Xanax': 1 }, '05': { 'MDMA': 2 }, '06': { 'Alcohol': 12 } },
     '04': {},
     '05': {},
   },
   '2024': {},
 };
-/*
-// Accessing an item in January 1st, 2023
-console.log(DATA['2023']['01']['01']['mdma']); // Output: 2
+//dict for units
+const units = {
+  'Marijuana': 'oz', 'Alcohol': 'drinks', 'Xanax': 'mg', 'MDMA': 'mg',
+}
+//day pos
+const numberWords = ['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth', 'Ninth', 'Tenth',
+  'Eleventh', 'Twelfth', 'Thirteenth', 'Fourteenth', 'Fifteenth', 'Sixteenth', 'Seventeenth', 'Eighteenth', 'Nineteenth',
+  'Twentieth', 'Twenty-first', 'Twenty-second', 'Twenty-third', 'Twenty-fourth', 'Twenty-fifth', 'Twenty-sixth',
+  'Twenty-seventh', 'Twenty-eighth', 'Twenty-ninth', 'Thirtieth', 'Thirty-first'
+];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+function getDayString(day: string) {
+  //YYYY-MM-DD
+  const components = day.split('-');
+  const M = parseInt(components[1]);
+  const D = parseInt(components[2]);
+  return (numberWords[D] + ' of ' + months[M]);
+}
 
+/*
 // Modifying an item in March 3rd, 2023
 DATA['2023']['03']['03']['alcohol'] = 5;
 console.log(DATA['2023']['03']['03']); // Output: { 'alcohol': 5, 'Marijuana': 1 }
 */
-
 
 /*
 TO DO later
